a3f50f0f1dd4c8e8dc48d8dc9acac67a72e9f3ef
ankur108 <ankur108@yahoo.co.uk>
2025-11-23 23:24:09 +0000

    Final TouchUp

--- src/utils/datetime_utils.py
+++ src/utils/datetime_utils.py
@@ -6,7 +6,7 @@ def timestamp_to_date_col(spark, df, timestamp_col, output_col):
     Extracts the date from a timestamp column and adds it as a new column in the DataFrame.
 
     Parameters:
-        spark: Spark Session.
+        spark: Spark Session. (This is Default)
         df (DataFrame): Input PySpark DataFrame containing the timestamp.
         timestamp_col (str): The name of the column containing the timestamp.
         output_col (str): The name for the output column with the ride date.
